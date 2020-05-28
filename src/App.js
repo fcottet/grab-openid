3b99f6e7b5002afe340e60c5d3bca3b7fb9af0aa
Francois Cottet <francois.cottet@grabtaxi.com>
2020-05-28 13:27:18 +0800

    Extract the subject from id_token JWT

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import './App.css';
 
 var jwt = require('jsonwebtoken');
 
+// eslint-disable-next-line
+function copyPuid() {
+  /* Get the text field */
+  var copyText = document.getElementById("puid");
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+}
 
 function App() {
   var token = "";
+  var decryptedToken ="";
+  var subject = "";
 
   var hash = window.location.hash.substr(1);
   console.log("hash="+hash);
@@ -16,20 +28,31 @@ function App() {
   if(index !== -1){ 
       token = hash.substr(index+tokenName.length);
       console.log("token="+token);
-
-      //var decryptedToken = token;
-      var decryptedToken = jwt.decode(token)
+      decryptedToken = jwt.decode(token);
+      subject = decryptedToken["sub"];
   }
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          This is the PartnerUserID:
+        </p>
+        <pre id="puid">{subject}</pre>
+
+        <p>
+          This is the decoded token:
+        </p>
+        <pre>{JSON.stringify(decryptedToken, null, 2) }</pre>
+
+        <br/>
+
         <p className="long">
           This is the fragment:<br/><br/>
           {hash}
         </p>
-        <p>
-          This is the decoded token:<br/><br/>
-          <pre>{JSON.stringify(decryptedToken, null, 2) }</pre>
+        <p className="long">
+          This is the full URL:<br/><br/>
+          {window.location.toString()}
         </p>
       </header>
     </div>
